5a2355fb6a4bc736f5755422642e43f412be1a1a
Josh Noel <joshnoel@mit.edu>
2016-07-16 19:59:24 -0400

    Fixed use of windows helper library regarding linking with .lib

--- include/vulint/vulkan_core.h
+++ include/vulint/vulkan_core.h
@@ -3,14 +3,38 @@
 
 #include <Windows.h>
 
+/**
+* Handles dynamic loading of vulkan library and functions
+*/
 class VulkanCore
 {
 public:
+	/**
+	* VulkanCore constructor.
+	* Loads the vulkan dll
+	*/
 	VulkanCore();
+
+	/**
+	* VulkanCore desctructor.
+	* Frees the vulkan dll handle
+	*/
 	~VulkanCore();
 
+	VulkanCore(const VulkanCore&) = delete;
+	VulkanCore& operator=(const VulkanCore&) = delete;
+
 private:
+	/**
+	* Handle to vulkan dll
+	*/
 	HMODULE vulkan_library_;
+
+	/**
+	* Loads function pointers for functions listed in vulkanFunctionList.h
+	* Then it declares the functions with their correct names using these pointers
+	*/
+	void InitFunctions();
 };
 
 #endif // !VULKAN_INTRO_VCORE_H
--- src/vulkan_core.cpp
+++ src/vulkan_core.cpp
@@ -1,15 +1,31 @@
 #include "vulint/vulkan_core.h"
+#include "winhelp/ErrorHandling.h"
 #include <iostream>
+#include <vulkan/vulkan.h>
+
+//functions are loaded dynamically
+//VK_NO_PROTOTYPES ensures vulkan function declarations are not made in "vulkan.h"
+//	this ensures functions not loaded from "vulkan-1.dll" are not declared.
+//	If static linking with vulkan every declaration in "vulkan.h" would be defined,
+//	but this is not true with dynamic linking
 
 VulkanCore::VulkanCore()
 {
 	vulkan_library_ = LoadLibrary("vulkan-1.dll");
+	InitFunctions();
 }
+
+void VulkanCore::InitFunctions()
+{
+	
+}
+
 
 VulkanCore::~VulkanCore()
 {
 	if (!FreeLibrary(vulkan_library_))
-	{
-		//TODO: Handle Error
+	{	
+		//TODO: Possibly display errors using message boxes instead
+		std::cerr << WinHelper::GetLastSystemError() << std::endl;
 	}
 }
